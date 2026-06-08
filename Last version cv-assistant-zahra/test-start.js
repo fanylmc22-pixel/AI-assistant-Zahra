4113aae9feb4acf109e4de525611c23410383e03
fanylmc22-pixel <fanylmc22@hotmail.com>
2026-06-08 21:16:07 +0200

    Delete Last version cv-assistant-zahra directory

--- Last version cv-assistant-zahra/test-start.js
+++
@@ -1,8 +0,0 @@
-import { exec } from 'child_process';
-const proc = exec('npm run dev');
-proc.stdout.on('data', console.log);
-proc.stderr.on('data', console.error);
-setTimeout(() => {
-   proc.kill();
-   console.log('killed');
-}, 5000);
